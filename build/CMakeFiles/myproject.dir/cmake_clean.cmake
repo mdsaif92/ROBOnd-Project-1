07e9eb1fd5094d6e1f824ec38a8540c14359a356
Saif Mohamed <mdsaif92@gmail.com>
2022-06-22 10:54:11 -0700

    first commit

---
+++ build/CMakeFiles/myproject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myproject.dir/script/myproject.cpp.o"
+  "libmyproject.pdb"
+  "libmyproject.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
